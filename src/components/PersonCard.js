6947fe8d15adfd7610b9ae3b244c7bca98741749
nesibeytskn <nesibeyetiskin1@gmail.com>
2023-11-14 23:31:15 +0300

    first commit

---
+++ src/components/PersonCard.js
@@ -0,0 +1,78 @@
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import React, {memo} from 'react';
+
+const PersonCard = ({personInfo, setTitle, setAge}) => {
+  //console.log(personInfo);
+  const {email, first_name, id, last_name, gender, photo} = personInfo;
+
+  return (
+    <TouchableOpacity style={styles.mainContainer} onPress={() => setAge(id+10)}>
+      <View style={styles.leftSide}>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={{
+              uri: photo,
+            }}
+          />
+        </View>
+        <View style={styles.infoConatainer}>
+          <Text onPress={() => setTitle(first_name)} style={styles.name}>
+            {first_name + '  ' + last_name}
+          </Text>
+          <Text style={styles.job}>{email}</Text>
+        </View>
+      </View>
+      <View style={styles.infoConatainer}>
+        <Text style={styles.name}>{gender}</Text>
+        <Text style={styles.job}>{id + 20}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default memo(PersonCard);
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    margin: 15,
+    borderRadius: 15,
+  },
+  leftSide: {
+    flexDirection: 'row',
+    gap: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  infoConatainer: {
+    marginBottom: 35,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: '800',
+    marginBottom: 10,
+    color: 'white',
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    objectFit: 'cover',
+  },
+
+  job: {
+    fontSize: 13,
+    color: '#6b7280',
+    fontWeight: '600',
+  },
+});
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,66 @@
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import React, {useState, useCallback, useMemo} from 'react';
+import PersonCard from '../components/PersonCard';
+import {data} from '../utils/constants';
+
+const HomeScreen = () => {
+  const [isGreen, setIsGreen] = useState(false);
+  const [title, setTitle] = useState('Kişi Listesi');
+  const [age, setAge] = useState(0);
+
+  const cachedSetAppTitle = useCallback(willSetTitle => {
+    for (let i = 0; i < 100000000; i++) {}
+
+    setTitle(willSetTitle);
+  }, []);
+
+  const handleSetAge = () => {
+    //console.log('fonksiyon çalıştı');
+    for (let i = 0; i < 100000000; i++) {}
+    return age + 10;
+  };
+  const cachedSetAge = useMemo(() => handleSetAge(), [age]);
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.headerContainer}>
+        <Text
+          style={[styles.title, {color: isGreen ? '#16c784' : 'white'}]}
+          onPress={() => setIsGreen(!isGreen)}>
+          {title}
+        </Text>
+        <Text style={styles.title}>{cachedSetAge}</Text>
+      </View>
+      <FlatList
+        data={data}
+        renderItem={({item}) => (
+          <PersonCard
+            personInfo={item}
+            setTitle={cachedSetAppTitle}
+            setAge={setAge}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#6b7280',
+  },
+  headerContainer: {
+    backgroundColor: 'black',
+    paddingHorizontal: 35,
+    paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: 'white',
+  },
+});
